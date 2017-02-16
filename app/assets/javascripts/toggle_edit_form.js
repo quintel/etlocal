6497e468b53b2a37b64089b0b54254a256f7acef
Gerard Westerhof <gerard@grdw.nl>
2017-02-16 11:56:27 +0100

    Fix cancel button for a single edit

--- app/assets/javascripts/toggle_edit_form.js
+++ app/assets/javascripts/toggle_edit_form.js
@@ -1,13 +1,15 @@
 $(document).on('turbolinks:load', function () {
     'use strict';
 
-    $("div.editable").on("click.enable", function () {
+    $("div.editable strong.key").on("click.enable", function () {
         $("div.editable").removeClass("enabled");
 
-        $(this).addClass("enabled");
+        $(this).parents(".editable").addClass("enabled");
     });
 
     $("div.editable .button.cancel").on("click.disable", function (e) {
         e.preventDefault();
+
+        $(this).parents(".editable").removeClass("enabled");
     });
 });

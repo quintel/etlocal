81d7f27aed8fea400423b50cce0d9e8ac2d6c351
Gerard Westerhof <gerard@grdw.nl>
2017-02-17 10:13:04 +0100

    Add a name field to the user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit({ roles: [] }, :name, :email, :password, :password_confirmation)
+    end
+  end
 end

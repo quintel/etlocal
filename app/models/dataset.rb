91fe4b9d27d4b6cf14c66618d9e57b565b9145b3
Gerard Westerhof <gerard@grdw.nl>
2017-02-21 13:49:27 +0100

    Adding quick tiny maps of the dataset

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,6 +1,12 @@
 class Dataset
   ATLAS_ATTRIBUTES = %i(base_dataset analysis_year area id)
 
+  DATASET_MAPS = {
+    drenthe:          { center: "52.971641,6.702942", zoom: 8 },
+    ameland:          { center: "53.447075,5.780356", zoom: 9 },
+    noorderplantsoen: { center: "53.224561,6.556155", zoom: 14 }
+  }
+
   attr_reader :atlas_dataset
 
   def self.all
@@ -23,6 +29,12 @@ class Dataset
         @atlas_dataset.public_send(attribute)
       end
     end
+  end
+
+  def map_image
+    options = DATASET_MAPS[area.to_sym]
+
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{ options[:center] }&zoom=#{ options[:zoom] }&size=200x200"
   end
 
   def editable_attributes

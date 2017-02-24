52fb935a9c115db440691cfe01ed62ece69513a5
Gerard Westerhof <gerard@grdw.nl>
2017-02-24 15:33:39 +0100

    Fix deploy configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,29 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
+set :log_level, 'info'
+
 set :application, "etlocal"
 set :repo_url, "git@github.com:quintel/etlocal.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
+# Set up rbenv
+set :rbenv_type, :user
+set :rbenv_ruby, '2.4.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs,
+  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+  after :publishing, :restart
+end

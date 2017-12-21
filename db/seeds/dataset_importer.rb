61250c0f8b5c3bd89530f4976f800dc84d3df2db
Gerard Westerhof <gerard@grdw.nl>
2017-12-21 13:56:30 +0100

    Fix the dataset importer

--- db/seeds/dataset_importer.rb
+++ db/seeds/dataset_importer.rb
@@ -1,24 +1,33 @@
+require 'progress_bar'
+
 class DatasetImporter
   def initialize(country = 'nl')
     @country = country
   end
 
   def import
-    dataset.each do |row|
+    bar = ProgressBar.new(datasets.size)
+
+    datasets.each do |row|
       dataset_row = DatasetCSVRow.new(row.to_h)
 
-      dataset = Dataset.find_or_create_by(geo_id: dataset_row.geo_id)
-      dataset.area = dataset_row.area
-      dataset.save
+      unless dataset = Dataset.find_by(geo_id: dataset_row.geo_id)
+        dataset = Dataset.find_or_create_by(geo_id: dataset_row.geo_id)
+        dataset.area = dataset_row.area
+        dataset.save
+      end
 
       Defaults.set(dataset, dataset_row)
+      bar.increment!
     end
   end
 
   private
 
-  def dataset
-    CSV.read(Rails.root.join("db", "datasets", "#{ @country }_datasets.csv"),
-      headers: true)
+  def datasets
+    @datasets ||= CSV.read(
+      Rails.root.join("db", "datasets", "#{ @country }_datasets.csv"),
+      headers: true
+    )
   end
 end

0f535a09c0296ba3afd1b69f42652c37aeae2916
Gerard Westerhof <gerard@grdw.nl>
2018-02-01 09:40:03 +0100

    Fix bug in change trigger

--- app/assets/javascripts/dataset_interface/change_trigger.js
+++ app/assets/javascripts/dataset_interface/change_trigger.js
@@ -1,15 +1,17 @@
 DatasetInterface.ChangeTrigger = {
     trigger: function (attribute) {
-        var showChanges = $('input.changed').length > 0;
-
-        $('input.changed').removeClass('valid invalid');
+        var showChanges;
 
         $(this).toggleClass("changed", $(this).val() !== '');
 
-        $(".overview-edits .apply.no-changes").toggle(showChanges);
+        showChanges = $('input.changed').length > 0;
+
+        $('input.changed').removeClass('valid invalid');
+
+        $(".overview-edits .apply.no-changes").toggle(!showChanges);
         $(".overview-edits .apply.changes")
             .removeClass('valid invalid')
-            .toggle(!showChanges);
+            .toggle(showChanges);
 
         DatasetInterface.FormEnabler.enable(attribute);
     }

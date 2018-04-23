c490eb6c816390bed544dcb10b15b921ca0bb1b1
Gerard Westerhof <gerard@grdw.nl>
2018-04-23 11:18:33 +0200

    Restructure the interface elements and add basic validations in the specs

--- app/models/interface_element.rb
+++ app/models/interface_element.rb
@@ -1,22 +1,39 @@
+class InterfaceItem
+  include Virtus.model
+  include ActiveModel::Validations
+
+  validates_presence_of :unit, :key
+
+  attribute :key, Symbol
+  attribute :unit, String
+  attribute :flexible, Boolean, default: false
+end
+
 class InterfaceGroup
   include Virtus.model
+  include ActiveModel::Validations
+
+  validates_presence_of :items
 
   attribute :header, String
   attribute :type, Symbol, default: :static
-  attribute :items, Hash[Symbol => String]
+  attribute :items, Array[InterfaceItem]
   attribute :subgroups, Array[InterfaceGroup]
 end
 
 class InterfaceElement < YmlReadOnlyRecord
   include Virtus.model
   include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  validates_presence_of :key
 
   attribute :key, Symbol
   attribute :groups, Array[InterfaceGroup]
 
   def self.keys
     InterfaceElement.all.flat_map(&:groups).flat_map do |group|
-      group.items.keys
+      group.items.map(&:key)
     end
   end
 end
---
+++ spec/models/interface_element_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe InterfaceElement do
+  InterfaceElement.all.each do |interface_element|
+    it "#{ interface_element.key } is valid" do
+      expect(interface_element).to be_valid
+    end
+
+    interface_element.groups.each do |interface_group|
+      it "#{ interface_group } is a valid interface group" do
+        expect(interface_group).to be_valid
+      end
+
+      interface_group.items.each do |interface_item|
+        it "#{ interface_item.key } is a valid interface item" do
+          expect(interface_item).to be_valid
+        end
+      end
+    end
+  end
+end

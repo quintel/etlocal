208271a8e448a0be004767870050dd40890898fa
Anthony Williams <hi@antw.io>
2018-08-22 17:15:30 +0100

    Clean up Exporter

--- app/services/exporter.rb
+++ app/services/exporter.rb
@@ -1,23 +1,21 @@
+# Fetches data from an ETLocal server, runs Transformer to create a
+# fully-functional dataset, and creates a dataset in a local copy of ETSource.
 module Exporter
-  require 'rest-client'
-
   DATASET_URL = "#{Rails.configuration.etsource_export_root}/api/v1".freeze
 
-  # Module that exports a set of changes from a dataset directly to changes
-  # inside of ETSource.
+  # Public: Exports an ETLocal dataset to an ETSource dataset.
   def self.export(dataset_ids)
-    begin
-      result = RestClient.get("#{DATASET_URL}/exports/#{dataset_ids}",
-                              accept: :json,
-                              content_type: :json)
+    response = RestClient.get(
+      "#{DATASET_URL}/exports/#{dataset_ids}",
+      accept: :json,
+      content_type: :json
+    )
 
-      JSON.parse(result).each do |dataset|
-        Transformer::DatasetGenerator.new(dataset).generate
-
-        puts "Successfully analyzed and exported #{dataset[:area]}"
-      end
-    rescue RestClient::ExceptionWithResponse => e
-      puts "Something went wrong with the analyzes and or exporting of #{dataset_ids}"
+    JSON.parse(response).each do |dataset|
+      Transformer::DatasetGenerator.new(dataset).generate
+      puts "Successfully analyzed and exported #{dataset[:area]}"
     end
+  rescue RestClient::ExceptionWithResponse
+    puts "Failed to fetch data for #{dataset_ids}"
   end
 end

04a6df1641935fb887bbb835ffb890003d8a7e5d
Michiel den Haan <michiel.denhaan@quintel.com>
2018-10-16 11:28:21 +0200

    updated municipality data to 2016, step 2 (updating missing data)

---
+++ db/migrate/20181016092655_munic_update_step2.rb
@@ -0,0 +1,28 @@
+class MunicUpdateStep2 < ActiveRecord::Migration[5.0]
+  def self.up
+    directory    = Rails.root.join('db/migrate/20181016092655_munic_update_step2')
+    data_path    = directory.join('data.csv')
+    commits_path = directory.join('commits.yml')
+    datasets     = []
+
+    CSVImporter.run(data_path, commits_path) do |row, runner|
+      print "Updating #{row['geo_id']}... "
+      commits = runner.call
+
+      if commits.any?
+        datasets.push(commits.first.dataset)
+        puts 'done!'
+      else
+        puts 'nothing to change!'
+      end
+    end
+
+    puts
+    puts "Updated #{datasets.length} datasets with the following IDs:"
+    puts "  #{datasets.map(&:id).join(',')}"
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end

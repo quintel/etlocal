cc8257a5e4227938c7512fef52ad63a763424848
Anthony Williams <hi@antw.io>
2019-01-17 14:11:14 +0000

    Round all group sliders when user makes change
    When changing a slider belonging to a group which has not previously
    been modified, those that are changed were rounded while those that were
    not still retained many decimal placed. This results in groups not
    summing to 100%.
    
    Until a better balancing solution can be created (like ETModel), now all
    sliders belonging to the group will be rounded, ensuring the group sums
    to 100% at the cost of modifying inputs which the user may not have
    explicitly touched.
    
    Closes #155

--- app/assets/javascripts/sliders/slider_group.js
+++ app/assets/javascripts/sliders/slider_group.js
@@ -17,6 +17,23 @@ var SliderGroup = (function () {
             if (this.isFlexGroup()) {
                 balancer = new $.Quinn.Balancer();
                 controls.forEach(balancer.add.bind(balancer));
+
+                // Update the value for each slider in the group; this is needed
+                // to ensure that default slider values -- which may contain
+                // many decimal places -- are replaced with rounded versions,
+                // ensuring the group still sums to 100%.
+                balancer.on('change', function() {
+                    // There are no subordinates when the event is fired after
+                    // the user stops dragging the slider. That's fine: the
+                    // group will have been set correctly by the previous
+                    // invocation of the callback.
+                    if (balancer.subordinates) {
+                        balancer.subordinates.forEach(function(slider) {
+                            slider.setValue(slider.model.value, false, true);
+                        });
+                    }
+                });
+
                 this.setFlexSlider();
             }
         },

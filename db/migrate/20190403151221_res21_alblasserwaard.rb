e8555ed67dec275edd63b4ceb389718d92155102
Roos de Kok <roos.dekok@quintel.com>
2019-04-03 17:35:59 +0200

    Add migration files for adding RES-regions

---
+++ db/migrate/20190403151221_res21_alblasserwaard.rb
@@ -0,0 +1,36 @@
+class Res21Alblasserwaard < ActiveRecord::Migration[5.0]
+  def self.up
+    directory    = Rails.root.join('db/migrate/20190403151221_res21_alblasserwaard')
+    data_path    = directory.join('data.csv')
+    commits_path = directory.join('commits.yml')
+    datasets     = []
+
+    # By default, CSVImporter only allows updating existing datasets. If the
+    # migration is adding a new dataset, add the `create_missing_datasets`
+    # keyword argument. For example:
+    #
+    #   CSVImporter.run(data_path, commits_path, create_missing_datasets: true) do |row, runner|
+    #     # ...
+    #   end
+    #
+    CSVImporter.run(data_path, commits_path, create_missing_datasets: true) do |row, runner|
+      print "Updating #{row['geo_id']}... "
+      commits = runner.call
+
+      if commits.any?
+        datasets.push(commits.first.dataset)
+        puts 'done!'
+      else
+        puts 'nothing to change!'
+      end
+    end
+
+    puts
+    puts "Updated #{datasets.length} datasets with the following IDs:"
+    puts "  #{datasets.map(&:id).join(',')}"
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end

b8b25d7433af2ee3eef97b006627ed4d1dcab48d
Anthony Williams <hi@antw.dev>
2020-03-24 15:58:35 +0000

    Update Rails from 5.0.7.2 to 5.2.34.2

--- Gemfile
+++ Gemfile
@@ -7,11 +7,13 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'bootsnap', require: false
+
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails', '>= 1.0.3'
 gem 'mysql2'
 gem 'puma', '~> 3.12'
-gem 'rails', '~> 5.0.1'
+gem 'rails', '~> 5.2'
 gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,69 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200310170035) do
+ActiveRecord::Schema.define(version: 2020_03_10_170035) do
 
-  create_table "commits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
-    t.integer  "source_id"
-    t.integer  "user_id"
-    t.text     "message",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "dataset_id"
-    t.index ["dataset_id"], name: "index_commits_on_dataset_id", using: :btree
-    t.index ["source_id"], name: "index_commits_on_source_id", using: :btree
-    t.index ["user_id"], name: "index_commits_on_user_id", using: :btree
+  create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.integer "source_id"
+    t.integer "user_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "dataset_id"
+    t.index ["dataset_id"], name: "index_commits_on_dataset_id"
+    t.index ["source_id"], name: "index_commits_on_source_id"
+    t.index ["user_id"], name: "index_commits_on_user_id"
   end
 
-  create_table "dataset_edits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
-    t.integer  "commit_id"
-    t.string   "key"
-    t.float    "value",      limit: 53
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.index ["commit_id"], name: "index_dataset_edits_on_commit_id", using: :btree
+  create_table "dataset_edits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.integer "commit_id"
+    t.string "key"
+    t.float "value", limit: 53
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commit_id"], name: "index_dataset_edits_on_commit_id"
   end
 
-  create_table "datasets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
-    t.integer  "user_id"
-    t.string   "name",            default: "",    null: false
-    t.string   "geo_id",          default: "",    null: false
-    t.string   "country",         default: "nl"
-    t.boolean  "has_industry",    default: false
-    t.boolean  "has_agriculture", default: false
-    t.boolean  "public",          default: true
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.index ["user_id"], name: "index_datasets_on_user_id", using: :btree
+  create_table "datasets", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name", default: "", null: false
+    t.string "geo_id", default: "", null: false
+    t.string "country", default: "nl"
+    t.boolean "has_industry", default: false
+    t.boolean "has_agriculture", default: false
+    t.boolean "public", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_datasets_on_user_id"
   end
 
-  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
-    t.string   "key"
+  create_table "groups", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sources", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "source_file_file_name"
-    t.string   "source_file_content_type"
-    t.integer  "source_file_file_size"
+  create_table "sources", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "source_file_file_name"
+    t.string "source_file_content_type"
+    t.integer "source_file_file_size"
     t.datetime "source_file_updated_at"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
-    t.integer  "group_id"
-    t.string   "email",                  limit: 191, default: "", null: false
-    t.string   "name"
-    t.string   "encrypted_password",                 default: "", null: false
-    t.string   "reset_password_token",   limit: 191
+  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.integer "group_id"
+    t.string "email", limit: 191, default: "", null: false
+    t.string "name"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token", limit: 191
     t.datetime "reset_password_sent_at"
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["group_id"], name: "index_users_on_group_id", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["group_id"], name: "index_users_on_group_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

dde770e6cc945becd0c8a8e133e8226227078808
Anthony Williams <hi@antw.dev>
2020-03-30 18:03:59 +0100

    Validate uniqueness of user e-mail
    Sentry: ETLOCAL-1 and ETLOCAL-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   validates_presence_of :email
   validates_email_format_of :email
+  validates_uniqueness_of :email
   validates_presence_of :name
   validates :password, presence: true, confirmation: true
 

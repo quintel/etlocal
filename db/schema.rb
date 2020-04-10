99fc9a6e58a23990e90aa5f1ff115075886733c6
marliekeverweij <marlieke.verweij@quintel.com>
2020-04-10 14:09:00 +0200

    Adding migration for the 5 Waddeneilanden 2017

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_080450) do
+ActiveRecord::Schema.define(version: 2020_04_10_115119) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

893ba2ee3e4902b96c6ab4e9233f6b11ef36e651
marliekeverweij <marlieke.verweij@quintel.com>
2020-10-22 15:49:35 +0200

    Add migration for adding Northern Ireland 2018

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_075207) do
+ActiveRecord::Schema.define(version: 2020_10_22_122300) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

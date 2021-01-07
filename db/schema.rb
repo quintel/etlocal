1d303d9f2bff754f946245b9a83ca82d4cdcbced
Michiel den Haan <michiel.denhaan@quintel.com>
2021-01-07 13:54:38 +0100

    Update Smallingerland to 2018

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_153410) do
+ActiveRecord::Schema.define(version: 2021_01_07_124953) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

c5728dfb38f0dd042420945b57c00aa30b331a8e
Nora Schinkel <ncschinkel@gmail.com>
2021-03-15 17:11:09 +0100

    Add images to precompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Compile images in /assets/images
+Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]

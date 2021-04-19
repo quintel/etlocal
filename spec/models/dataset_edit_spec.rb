f8ccc68fd56abb12793707fcfc157a717f022e24
Nora Schinkel <ncschinkel@gmail.com>
2021-04-19 13:33:44 +0200

    Swap Robot for Quintel in historical changes

--- spec/models/dataset_edit_spec.rb
+++ spec/models/dataset_edit_spec.rb
@@ -1,26 +1,45 @@
 require 'rails_helper'
 
 describe DatasetEdit do
-  before { dataset.valid? }
+  before { dataset_edit.valid? }
 
   describe "blank dataset edit" do
-    let(:dataset) { DatasetEdit.new }
+    let(:dataset_edit) { DatasetEdit.new }
 
     it "can't have a blank value" do
-      expect(dataset).to have(1).errors_on(:value)
+      expect(dataset_edit).to have(1).errors_on(:value)
     end
 
     it "can't have a blank key" do
-      expect(dataset).to have(1).errors_on(:key)
+      expect(dataset_edit).to have(1).errors_on(:key)
     end
   end
 
   describe "must be bigger than or 0" do
     describe "for regular attribute" do
-      let(:dataset) { DatasetEdit.new(value: 0) }
+      let(:dataset_edit) { DatasetEdit.new(value: 0) }
 
       it "is invalid" do
-        expect(dataset).to have(0).errors_on(:value)
+        expect(dataset_edit).to have(0).errors_on(:value)
+      end
+    end
+  end
+
+  describe '#creator' do
+    let(:dataset_edit) { DatasetEdit.new(value: 0, commit: commit) }
+    let(:commit) { FactoryBot.create(:commit, user: user) }
+
+    context 'with a creator without a group' do
+      let(:user) { FactoryBot.create(:user, group: nil) }
+      it 'is the users name' do
+        expect(dataset_edit.creator).to eq(user.name)
+      end
+    end
+
+    context 'with a creator without a group' do
+      let(:user) { FactoryBot.create(:user) }
+      it 'is the users name' do
+        expect(dataset_edit.creator).to eq(user.group.key.humanize)
       end
     end
   end

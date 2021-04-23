6125511b61283a7e80a29e690836341f28d2e4cb
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2021-04-23 14:42:23 +0200

    Combine municipalities for province Utrecht 2018

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_073555) do
+ActiveRecord::Schema.define(version: 2021_04_23_120159) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

9c0ff88940dd43f2167128af16004977d4cae885
redekok <roos.dekok@quintel.com>
2021-04-26 09:33:13 +0200

    Aggregate datasets based on production instead of beta server

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_120159) do
+ActiveRecord::Schema.define(version: 2021_04_26_072509) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

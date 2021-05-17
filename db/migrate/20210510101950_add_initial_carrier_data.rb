88dd090f1418f0a1b8ce4993dadb46c08ff51510
Anthony Williams <hi@antw.dev>
2021-05-17 15:09:08 +0100

    Add dataset-specific carrier values
    Carrier values differ depending sometimes on the region, but most often
    on the country in which the region is located. This commti rewrites the
    (as yet, undeployed) migration adding carrier values so that each
    dataset gets values consistent with the old FCE files in ETSource.

--- db/migrate/20210510101950_add_initial_carrier_data.rb
+++ db/migrate/20210510101950_add_initial_carrier_data.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
+# Adds region-specific carrier data.
 class AddInitialCarrierData < ActiveRecord::Migration[5.2]
   Cell = Struct.new(:carrier, :key, :value) do
     def interface_key
-      "files_carriers_#{carrier}_#{key}"
+      "file_carriers_#{carrier}_#{key}"
     end
   end
 
   def up
-    carrier_values = build_carrier_values
-
     message = <<-MSG.strip_heredoc
       Add emissions and costs for energy carriers using data for the Netherlands.
     MSG
@@ -18,6 +19,7 @@ class AddInitialCarrierData < ActiveRecord::Migration[5.2]
       len_width  = length.to_s.length
 
       datasets.find_each.with_index do |dataset, index|
+        region_used, carrier_values = carrier_values_for(dataset.geo_id)
         commit = dataset.commits.build(message: message, user: User.robot)
 
         carrier_values.each do |cv|
@@ -28,7 +30,7 @@ class AddInitialCarrierData < ActiveRecord::Migration[5.2]
 
         say(
           "#{format("%#{len_width}d", index + 1)}/#{length}: " \
-          "#{dataset.geo_id} #{dataset.name} (#{dataset.id})"
+          "#{dataset.geo_id} #{dataset.name} using data for #{region_used}"
         )
       end
     end
@@ -36,7 +38,7 @@ class AddInitialCarrierData < ActiveRecord::Migration[5.2]
 
   def down
     say_with_time('Removing carrier data') do
-      carrier_values = build_carrier_values
+      _, carrier_values = carrier_values_for(:nl)
       keys = carrier_values.map(&:interface_key)
 
       dataset_edits = DatasetEdit.select(%i[id commit_id]).where(key: keys)
@@ -57,11 +59,39 @@ class AddInitialCarrierData < ActiveRecord::Migration[5.2]
 
   private
 
+  def carrier_values_for(region_code)
+    @carrier_values ||= build_carrier_values
+
+    region_code = region_code.to_sym
+
+    return [region_code, @carrier_values[region_code]] if @carrier_values.key?(region_code)
+
+    dataset = Dataset.find_by!(geo_id: region_code)
+    atlas_key = dataset.atlas_key.to_sym
+
+    return [atlas_key, @carrier_values[atlas_key]] if @carrier_values.key?(atlas_key)
+
+    parent = dataset.base_dataset.to_sym
+
+    if @carrier_values[parent]
+      [parent, @carrier_values[parent]]
+    else
+      [:nl, @carrier_values[:nl]]
+    end
+  end
+
   def build_carrier_values
-    table = CSV.table(
-      Rails.root.join('db/migrate/20210510101950_add_initial_carrier_data/carriers.csv'),
-      headers: true
-    )
+    say_with_time('Building carrier values') do
+      files = Rails.root.join('db/migrate/20210510101950_add_initial_carrier_data').glob('*.csv')
+
+      files.each_with_object({}) do |path, data|
+        data[path.basename.sub_ext('').to_s.to_sym] = build_values_from_path(path)
+      end
+    end
+  end
+
+  def build_values_from_path(path)
+    table = CSV.table(Rails.root.join(path), headers: true)
 
     table.flat_map do |row|
       carrier = row[:key]

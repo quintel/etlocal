44d91de8c1cf02ce018f056257f84b827b31058c
Charlotte von Meijenfeldt <charlotte.vonmeijenfeldt@quintel.com>
2021-06-16 14:28:48 +0200

    Update of Leeuwarden municipality to 2018 GM0080

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_085203) do
+ActiveRecord::Schema.define(version: 2021_06_16_120917) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

c3442caefb06f352620d86efc1f16b16a1456228
Charlotte von Meijenfeldt <charlotte.vonmeijenfeldt@quintel.com>
2021-07-30 13:55:29 +0200

    Update Steenbergen dataset to 2018

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_125039) do
+ActiveRecord::Schema.define(version: 2021_07_30_114148) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

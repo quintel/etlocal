2af3fe45c97dccb19ea461387761bb90e8e96a9b
Anthony Williams <hi@antw.dev>
2021-12-23 14:54:56 +0000

    Add Docker configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.etsource_path = Rails.root.join('etsource')
+  config.etsource_path = Pathname.new(ENV.fetch('ETSOURCE_PATH', '/etsource'))
 end

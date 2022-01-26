b09e78be49f7830587a8e78417e259f405884923
Charlotte von Meijenfeldt <charlotte.vonmeijenfeldt@quintel.com>
2022-01-26 13:24:18 +0100

    adding lignite chp to municipal datasets

--- db/migrate/20220126084957_lignite_production.rb
+++ db/migrate/20220126084957_lignite_production.rb
@@ -1,30 +1,22 @@
 class LigniteProduction < ActiveRecord::Migration[5.2]
 
   def up
-    Dataset.find_each do |dataset|
-      if dataset.group != 'municipality'
-        ActiveRecord::Base.transaction do
-          com = Commit.create!(
-            user_id: 4,
-            dataset_id: dataset.id,
-            message: 'Set to 0.0 as there is no energy production using lignite present in this area.'
-          )
-          create_edit(com, input_energy_chp_ultra_supercritical_lignite_production, 0.0)
-        end
+    Dataset.where(data_source: 'db').find_each do |dataset|
+      next if dataset.group == 'country'
+
+      ActiveRecord::Base.transaction do
+        commit = Commit.create!(
+          user_id: 4,
+          dataset_id: dataset.id,
+          message: 'Set to 0.0 as there is no energy production using lignite present in this area.'
+        )
+
+        DatasetEdit.create!(
+          commit_id: commit.id,
+          key: 'input_energy_chp_ultra_supercritical_lignite_production',
+          value: 0.0
+        )
       end
-    end
-  end
-
-  private
-
-  # Create a new dataset edit
-  def create_edit(commit, key, value)
-    ActiveRecord::Base.transaction do
-      DatasetEdit.create!(
-        commit_id: commit.id,
-        key: key,
-        value: value
-      )
     end
   end
 end

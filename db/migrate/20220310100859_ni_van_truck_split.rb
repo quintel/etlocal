2258fab52dd5ab1223165a366b722706b5e643b7
Charlotte von Meijenfeldt <charlotte.vonmeijenfeldt@quintel.com>
2022-03-10 14:22:39 +0100

    NI adding vans and truck split

---
+++ db/migrate/20220310100859_ni_van_truck_split.rb
@@ -0,0 +1,43 @@
+class NiVanTruckSplit < ActiveRecord::Migration[5.0]
+  def self.up
+    directory    = Rails.root.join('db/migrate/20220310100859_ni_van_truck_split')
+    data_path    = directory.join('data.csv')
+    commits_path = directory.join('commits.yml')
+    datasets     = []
+
+    # By default, CSVImporter only allows updating existing datasets. If the
+    # migration is adding a new dataset, add the `create_missing_datasets`
+    # keyword argument. For example:
+    #
+    #   CSVImporter.run(data_path, commits_path, create_missing_datasets: true) do |row, runner|
+    #     # ...
+    #   end
+    #
+    CSVImporter.run(data_path, commits_path) do |row, runner|
+      print "Updating #{row['geo_id']}... "
+      commits = runner.call
+
+      if commits.any?
+        datasets.push(find_dataset(commits))
+        puts 'done!'
+      else
+        puts 'nothing to change!'
+      end
+    end
+
+    sleep(1)
+    puts
+    puts "Updated #{datasets.length} datasets with the following IDs:"
+    puts "  #{datasets.map(&:id).join(',')}"
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+  def find_dataset(commits)
+    commits.each do |commit|
+      return commit.dataset if commit&.dataset
+    end
+  end
+end

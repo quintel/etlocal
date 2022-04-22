fd6370dbaa5b1c0184360830273b5d2a9c369aed
marliekeverweij <marlieke.verweij@quintel.com>
2022-04-22 17:44:06 +0200

    EU migration for flh of chp and pp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_095353) do
+ActiveRecord::Schema.define(version: 2022_04_22_095226) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

f22a7f75bdd0b0a06ed7e0d0d9c26ec25e70d4c9
marliekeverweij <marlieke.verweij@quintel.com>
2022-08-05 11:08:43 +0200

    Northern-Ireland: update shares households and transport

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_090012) do
+ActiveRecord::Schema.define(version: 2022_08_05_074407) do
 
   create_table "commits", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"

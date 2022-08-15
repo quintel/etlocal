0173c0ea8c4d6d1fc6ced8982fab9da36479d8ae
Charlottevm <charlotte.vonmeijenfeldt@quintel.com>
2022-08-15 13:46:26 +0200

    foodvalley update to 2019

--- db/migrate/20220727083528_res05_foodvalley.rb
+++ db/migrate/20220727083528_res05_foodvalley.rb
@@ -1,6 +1,6 @@
 class Res05Foodvalley < ActiveRecord::Migration[5.0]
   def self.up
-    directory    = Rails.root.join('db/migrate/20190403143359_res05_foodvalley')
+    directory    = Rails.root.join('db/migrate/20220727083528_res05_foodvalley')
     data_path    = directory.join('data.csv')
     commits_path = directory.join('commits.yml')
     datasets     = []
@@ -18,19 +18,48 @@ class Res05Foodvalley < ActiveRecord::Migration[5.0]
       commits = runner.call
 
       if commits.any?
-        datasets.push(commits.first.dataset)
+        datasets.push(find_dataset(commits))
         puts 'done!'
       else
         puts 'nothing to change!'
       end
     end
 
+    sleep(1)
     puts
     puts "Updated #{datasets.length} datasets with the following IDs:"
     puts "  #{datasets.map(&:id).join(',')}"
+  rescue ActiveRecord::RecordInvalid => e
+    if e.record.is_a?(Commit) && e.record.errors['dataset_edits.value'].any?
+      warn('')
+      warn('-' * 80)
+      warn('The following errors occurred while processing CSV rows:')
+      warn('')
+
+      # Grab all the errors from individual datasets to show those instead. This is typically
+      # the case when a CSV specifies a value that is not allowed to be edited.
+      e.record
+        .dataset_edits
+        .reject(&:valid?)
+        .each do |edit|
+          edit.errors.each do |field, msg|
+            warn("* #{edit.commit.dataset.geo_id}: #{edit.key}: #{field} #{msg}")
+          end
+        end
+
+      warn('-' * 80)
+    end
+
+    raise e
   end
 
   def self.down
     raise ActiveRecord::IrreversibleMigration
+  end
+
+  def find_dataset(commits)
+    commits.each do |commit|
+      return commit.dataset if commit&.dataset
+    end
   end
 end

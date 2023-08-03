72917ff3e6bf550d6e28f6e6c4a473ea63546d7d
Thomas <thomas.hassing@quintel.com>
2023-08-03 15:03:26 +0200

    Add ORM-like functionality to Interface* models

--- app/models/interface_element.rb
+++ app/models/interface_element.rb
@@ -10,7 +10,11 @@ class InterfaceElement < YmlReadOnlyRecord
   attribute :key, Symbol
   attribute :groups, Array[InterfaceGroup]
 
+  def self.groups
+    @groups ||= all.flat_map(&:groups).compact
+  end
+
   def self.items
-    @items ||= all.flat_map(&:groups).flat_map(&:items)
+    @items ||= all.flat_map(&:groups).flat_map(&:items).compact
   end
 end

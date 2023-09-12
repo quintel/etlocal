875522c27d2c7b9be55e02fed48f9b6ff287b2cf
Roos de Kok <roos.dekok@quintel.com>
2023-09-12 16:31:16 +0200

    Add and update keys that should be touched by the dataset migration for heat improvements

--- db/migrate/20230830153143_rename_interface_element_keys_heat_2023.rb
+++ db/migrate/20230830153143_rename_interface_element_keys_heat_2023.rb
@@ -2,6 +2,7 @@ class RenameInterfaceElementKeysHeat2023 < ActiveRecord::Migration[5.0]
 
   # old_key => new_key
   KEYS = {
+    # production
     'input_energy_chp_ultra_supercritical_coal_production' => 'input_energy_chp_ultra_supercritical_ht_coal_production',
     'input_energy_chp_ultra_supercritical_cofiring_coal_production' => 'input_energy_chp_ultra_supercritical_cofiring_ht_coal_production',
     'input_energy_chp_ultra_supercritical_lignite_production' => 'input_energy_chp_ultra_supercritical_ht_lignite_production',
@@ -10,7 +11,7 @@ class RenameInterfaceElementKeysHeat2023 < ActiveRecord::Migration[5.0]
     'input_energy_chp_supercritical_waste_mix_production' => 'input_energy_chp_supercritical_ht_waste_mix_production',
     'input_energy_chp_local_wood_pellets_production' => 'input_energy_chp_local_ht_wood_pellets_production',
     'input_energy_chp_local_engine_biogas_production' => 'input_energy_chp_local_engine_ht_biogas_production',
-    'input_energy_heat_well_geothermal_production' => 'input_energy_heat_well_ht_geothermal_production',
+    'input_energy_heat_well_geothermal_production' => 'input_energy_heat_well_deep_ht_geothermal_production',
     'input_energy_heat_burner_wood_pellets_production' => 'input_energy_heat_burner_ht_wood_pellets_production',
     'input_energy_heat_burner_waste_mix_production' => 'input_energy_heat_burner_ht_waste_mix_production',
     'input_energy_heat_burner_hydrogen_production' => 'input_energy_heat_burner_ht_hydrogen_production',
@@ -18,7 +19,25 @@ class RenameInterfaceElementKeysHeat2023 < ActiveRecord::Migration[5.0]
     'input_energy_heat_boiler_electricity_production' => 'input_energy_heat_boiler_ht_electricity_production',
     'input_energy_heat_burner_network_gas_production' => 'input_energy_heat_burner_ht_network_gas_production',
     'input_energy_heat_burner_coal_production' => 'input_energy_heat_burner_ht_coal_production',
-    'input_energy_heat_burner_crude_oil_production' => 'input_energy_heat_burner_ht_crude_oil_production'
+    'input_energy_heat_burner_crude_oil_production' => 'input_energy_heat_burner_ht_crude_oil_production',
+    'input_energy_heat_solar_thermal_production' => 'input_energy_heat_solar_mt_solar_thermal_production',
+    
+    # full load hours
+    'energy_chp_local_engine_network_gas_full_load_hours' => 'input_energy_chp_local_engine_network_gas_full_load_hours', 
+    'energy_chp_combined_cycle_network_gas_full_load_hours' => 'input_energy_chp_combined_cycle_network_gas_full_load_hours', 
+    'energy_chp_ultra_supercritical_cofiring_coal_full_load_hours' => 'input_energy_chp_ultra_supercritical_cofiring_coal_full_load_hours', 
+    'energy_chp_ultra_supercritical_coal_full_load_hours' => 'input_energy_chp_ultra_supercritical_coal_full_load_hours', 
+    'energy_chp_ultra_supercritical_lignite_full_load_hours' => 'input_energy_chp_ultra_supercritical_lignite_full_load_hours', 
+    'energy_chp_local_wood_pellets_full_load_hours' => 'input_energy_chp_local_wood_pellets_full_load_hours', 
+    'energy_chp_local_engine_biogas_full_load_hours' => 'input_energy_chp_local_engine_biogas_full_load_hours', 
+    'energy_chp_supercritical_waste_mix_full_load_hours' => 'input_energy_chp_supercritical_waste_mix_full_load_hours', 
+    'energy_heat_burner_coal_full_load_hours' => 'input_energy_heat_burner_coal_full_load_hours', 
+    'energy_heat_burner_crude_oil_full_load_hours' => 'input_energy_heat_burner_crude_oil_full_load_hours', 
+    'energy_heat_burner_network_gas_full_load_hours' => 'input_energy_heat_burner_network_gas_full_load_hours', 
+    'energy_heat_burner_wood_pellets_full_load_hours' => 'input_energy_heat_burner_wood_pellets_full_load_hours', 
+    'energy_heat_solar_thermal_full_load_hours' => 'input_energy_heat_solar_solar_thermal_full_load_hours', 
+    'energy_heat_burner_waste_mix_full_load_hours' => 'input_energy_heat_burner_waste_mix_full_load_hours', 
+    'energy_heat_heatpump_water_water_electricity_full_load_hours' => 'input_energy_heat_heatpump_water_water_electricity_full_load_hours'
   }.freeze
 
   def self.up

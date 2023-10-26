edfdea56e3866c57f772e304c6dad498f7294bd5
Charlotte vm <charlottevm@Charlottes-MacBook-Air.local>
2023-10-26 15:30:32 +0200

    resolving migration

--- db/migrate/20231026093802_e_boiler_efficiency.rb
+++ db/migrate/20231026093802_e_boiler_efficiency.rb
@@ -1,29 +1,39 @@
 class EBoilerEfficiency < ActiveRecord::Migration[7.0]
   
+  EFFICIENCIES = {
+    'input_energy_heat_boiler_electricity_steam_hot_water_output_conversion' => 99.5
+  }.freeze
+
   def up
-    say_with_time('Setting e-boiler efficiency to 0.995 for all datasets') do
-      counter = 0
-      Dataset.all.find_each do |dataset|
-        next if dataset.queryable_source?
+    say "Checking and migrating #{Dataset.count} datasets"
+    changed = 0
 
+    Dataset.find_each do |dataset|
         ActiveRecord::Base.transaction do
-          commit = Commit.create!(
-            user_id: 4,
+          com = Commit.create!(
+            user: User.robot,
             dataset_id: dataset.id,
-            message: 'Based on the general efficiency of e-boilers, not region specific.'
+            message: 'Efficiency researched by Quintel. Source data can be found below the slider of this plant in the ETM front-end'
           )
 
-          DatasetEdit.create!(
-            commit_id: commit.id,
-            key: 'input_energy_heat_boiler_electricity_steam_hot_water_output_conversion',
-            value: 99.5
-          )
-
-          counter += 1
+          EFFICIENCIES.each do |key, value|
+            create_edit(com, key, value)
+          end
         end
-      end
-      counter
+
+      changed += 1
+    end
+    say "Finished (#{changed} updated)"
+  end
+
+  # Create a new dataset edit
+  def create_edit(commit, key, value)
+    ActiveRecord::Base.transaction do
+      DatasetEdit.create!(
+        commit_id: commit.id,
+        key: key,
+        value: value
+      )
     end
   end
 end
-

3c24422a0c2ab4305891754019ff70e80cc4c28d
Koen van Bemmelen <koen.vanbemmelen@quintel.com>
2024-03-08 08:41:37 +0100

    Add Eemsdelta municipality 2019 dataset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_23_114314) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_07_130443) do
   create_table "commits", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"
     t.integer "user_id"

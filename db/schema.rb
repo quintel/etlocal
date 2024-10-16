ba5d56bd2a458ff61d7bfa49339b387b9b3a8c51
Kas Kranenburg <kas.kranenburg@quintel.com>
2024-10-16 15:12:25 +0200

    Adding eb killer migration for powerplants outside energy production tab

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_14_162335) do
-
+ActiveRecord::Schema[7.0].define(version: 2024_10_16_122247) do
   create_table "commits", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"
     t.integer "user_id"

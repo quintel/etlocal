3a6f7b3229d971992a83ed57c58491acbe515b90
louispt1 <louisparkestalbot1@gmail.com>
2025-06-18 10:57:20 +0200

    Try the other way round

--- app/services/amalgamator/processor/combine.rb
+++ app/services/amalgamator/processor/combine.rb
@@ -16,9 +16,10 @@ module Amalgamator
       # 3. If flexible is true for an InterfaceItem, make it fill out the share of the group it belongs to
       def perform(datasets)
         combined_item_values = combine_item_values(datasets)
-        round_item_values(combined_item_values)
         adjust_flexible_shares(combined_item_values)
+        round_item_values(combined_item_values)
       end
+
 
       # Creates a hash with the item's keys as keys and the combined values of all datasets as values, e.g.:
       # {

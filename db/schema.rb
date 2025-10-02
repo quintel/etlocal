2cd809cfefaeeb6d7fb06300d2b03c4f01fc9566
louispt1 <louisparkestalbot1@gmail.com>
2025-10-02 15:49:12 +0200

    Remove migrations and schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_01_120012) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_25_153920) do
   create_table "commits", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"
     t.integer "user_id"

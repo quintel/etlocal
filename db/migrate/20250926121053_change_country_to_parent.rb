27f508581f4350f671adc8540af22e77a03c563a
Nora Schinkel <ncschinkel@gmail.com>
2025-10-13 11:22:27 +0200

    Cancel renamed migration so it will not be rerun

--- db/migrate/20250926121053_change_country_to_parent.rb
+++ db/migrate/20250926121053_change_country_to_parent.rb
@@ -1,5 +1,7 @@
 class ChangeCountryToParent < ActiveRecord::Migration[7.2]
   def change
-    rename_column :datasets, :country, :parent
+    return
+
+    rename_column(:datasets, :country, :parent)
   end
 end

ef216604acd39635e96b8377995bab9c40541a30
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2025-11-05 15:17:41 +0100

    Update aluminium production and demands for NO and RS #640

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_09_140536) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_05_141346) do
   create_table "commits", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "source_id"
     t.integer "user_id"

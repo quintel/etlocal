210265abb3c9105a3d3c57325e1d97876d72e9ad
claudiavalkenier <claudia.valkenier@quintel.com>
2026-09-17 15:49:57 +0200

    Add Island of Ireland to regions (otherwise it is grouped under provinces)

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -47,7 +47,7 @@ class Dataset < ApplicationRecord
       'district'
     elsif geo_id.start_with?('BU', 'BEBU')
       'neighbourhood'
-    elsif geo_id.start_with?('RG')
+    elsif geo_id.start_with?('RG','II')
       'region'
     elsif entso_data_source? || geo_id.start_with?('UKNI') || geo_id.start_with?('GB')
       'country'
